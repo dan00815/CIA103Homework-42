5c4db6171e0deb2169871e49db1b4d8d35d71c6a
Benson Wu <a0989159280@gmail.com>
2024-09-05 00:25:37 +0800

    hw6 first try

---
+++ src/hw6/CalTest.java
@@ -0,0 +1,41 @@
+package hw6;
+
+import java.util.Scanner;
+
+public class CalTest {
+
+	public static void main(String[] args) {
+		boolean res = false;
+		boolean check;
+
+		while (!res) {
+
+			try {
+				Calculator cal = new Calculator();
+				Scanner sc = new Scanner(System.in);
+				System.out.println("請輸入X");
+				check = sc.hasNextInt();
+				if (!check) {
+					throw new CalException("輸入格式錯誤");
+				}
+				int x = sc.nextInt();
+
+				System.out.println("請輸入Y");
+				check = sc.hasNextInt();
+				if (!check) {
+					throw new CalException("輸入格式錯誤");
+				}
+				int y = sc.nextInt();
+
+				cal.powerXY(x, y);
+				res = true;
+
+			} catch (CalException err) {
+				System.out.println(err.getMessage());
+			}
+
+		}
+
+	}
+
+}
